c936851d4737f1db945e42b36e08d8bab0e265f1
anaszone <mohammedanaschowdhury@gmail.com>
2025-09-27 11:36:56 +0600

    Rewrite the loadcategories part

--- script/index.js
+++ script/index.js
@@ -1,31 +1,50 @@
+// // 1. Ageee API ta load korthe hbe function diye
 
+// // loadCategories function start
+// function loadCategories(){
+//     //1. Fetch korthe hbe
+//     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+//     //2. Promise takee json() e convert korthe  hbe
+//     .then((res)=>res.json())
+//     //3. Then Again promise takee data tee convert kore niye asboo
+//     .then(data=> displayCategories(data.categories));
+// }
+// loadCategories();
 
-// 1. Ageee API ta load korthe hbe function diye
+// function displayCategories(categories){
+//     //1. Get the container
+//     const categoryContainer = document.getElementById("category-container");
+//     //2. Loop operation of [array of object]
+//     for( let cat of categories )
+//     {
+//         //3. Create an element
+//         const categoryDiv = document.createElement("div");
+//         categoryDiv.innerHTML = `
+//          <button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
+//         `
+//         //4. Append the element into the mother section.
+//         categoryContainer.appendChild(categoryDiv);
+//     }
+// }
+// // loadCategories function end
 
-// loadCategories function start
-function loadCategories(){
-    //1. Fetch korthe hbe
-    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    //2. Promise takee json() e convert korthe  hbe
-    .then((res)=>res.json())
-    //3. Then Again promise takee data tee convert kore niye asboo
-    .then(data=> displayCategories(data.categories));
+function loadCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  .then((res) => res.json())
+  .then(data=>displayCategories(data.categories));
 }
 loadCategories();
 
-function displayCategories(categories){
-    //1. Get the container
+function displayCategories(categories) {
     const categoryContainer = document.getElementById("category-container");
-    //2. Loop operation of [array of object]
+
     for( let cat of categories )
     {
-        //3. Create an element
         const categoryDiv = document.createElement("div");
         categoryDiv.innerHTML = `
-         <button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
+        <button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
         `
-        //4. Append the element into the mother section.
-        categoryContainer.appendChild(categoryDiv);
+        categoryContainer.append(categoryDiv);
+        
     }
 }
-// loadCategories function end
