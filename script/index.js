1827cfec6c19f97b171cffa7a1178b0661b264ad
anaszone <mohammedanaschowdhury@gmail.com>
2025-09-27 12:12:49 +0600

    dinamically load all videos

--- script/index.js
+++ script/index.js
@@ -33,7 +33,15 @@ function loadCategories() {
   .then((res) => res.json())
   .then(data=>displayCategories(data.categories));
 }
-loadCategories();
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then(data => displayVideos(data.videos));
+}
+
+
+
+
 
 function displayCategories(categories) {
     const categoryContainer = document.getElementById("category-container");
@@ -48,3 +56,37 @@ function displayCategories(categories) {
         
     }
 }
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach((video)  => {
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML = `
+
+        <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+        `
+        videoContainer.append(videoCard);
+    })
+}
+
+
+
+
+loadCategories();
+loadVideos();
